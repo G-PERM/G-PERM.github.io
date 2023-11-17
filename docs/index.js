7a660a7ff304333e51a80f6c3d9b1eb4cd5ea932
G-PERM <142655273+G-PERM@users.noreply.github.com>
2023-11-17 21:51:46 +0530

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,3 @@
 function WEBSITE() {window.location.href = "index.html";}
-function POCKET() {window.location.href = "";}
-function MAGIC() {window.location.href = "";}
+function POCKET() {window.location.href = "https://g-perm.github.io/2 BY 2/index.html";}
+function MAGIC() {window.location.href = "https://g-perm.github.io/3 BY 3/index.html";}
