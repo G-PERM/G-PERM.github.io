1a0cdd14de648ffd333eb9df6c5126049f5157fc
G-PERM <gpermweb@gmail.com>
2023-11-17 22:28:16 +0530

    NO %20

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,3 @@
 function WEBSITE() {window.location.href = "index.html";}
-function POCKET() {window.location.href = "2 BY 2/index.html";}
-function MAGIC() {window.location.href = "3 BY 3/index.html";}
+function POCKET() {window.location.href = "POCKET/index.html";}
+function MAGIC() {window.location.href = "MAGIC/index.html";}
