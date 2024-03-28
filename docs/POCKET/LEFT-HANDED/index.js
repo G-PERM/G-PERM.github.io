7ebddf6e56e9c7674722292a983dceaed049c989
G-PERM <gpermweb@gmail.com>
2024-03-28 20:22:17 +0530

    quite the change

--- docs/POCKET/LEFT-HANDED/index.js
+++ docs/POCKET/LEFT-HANDED/index.js
@@ -1,1 +1,4 @@
 function WEBSITE() {window.location.href = "https://g-perm.github.io";}
+function BEGINNER() {window.location.href = "BEGINNER/index.html";}
+function INTERMEDIATE() {window.location.href = "INTERMEDIATE/index.html";}
+function ADVANCED() {window.location.href = "ADVANCED/index.html";}
