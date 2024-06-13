7350b3ac0373856bcd0c0483f139fac6e08c0229
G-PERM <gpermweb@gmail.com>
2024-06-13 23:47:07 +0530

    NEW STUFF!

--- docs/POCKET/LEFT-HANDED/BEGINNER/index.js
+++ docs/POCKET/LEFT-HANDED/BEGINNER/index.js
@@ -1,1 +1,5 @@
 function WEBSITE() {window.location.href = "https://g-perm.github.io";}
+function BM() {window.location.href = "ORGETA/index.html"}
+function OAAHBM() {window.location.href = "ORTEGA/index.html"}
+function SBM() {window.location.href = "LBL/index.html"}
+function SM() {window.location.href = "UNKNOWN/index.html"}
