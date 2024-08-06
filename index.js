99c16f9d1331c0a8f49367ac691a82695234dad6
gperm <gpermweb@gmail.com>
2024-08-06 22:32:31 +0530

    HVFGDYIFSGFYJFASUDVSYDF

---
+++ index.js
@@ -0,0 +1,18 @@
+function web() {
+ window.location.assign("index.html")
+}
+
+function NAVtop() {
+ document.getElementById("NAVnottop").style.color = "#00FFFF";
+ document.getElementById("NAVtop").style.color = "#FF0000";
+ document.getElementById("NAVnottop").style.backgroundColor = "#979797";
+ document.getElementById("NAVtop").style.backgroundColor = "#303030";
+ document.getElementById("NAV").style.position = "sticky";
+}
+function NAVnottop() {
+ document.getElementById("NAVtop").style.color = "#00FFFF";
+ document.getElementById("NAVnottop").style.color = "#FF0000";
+ document.getElementById("NAVtop").style.backgroundColor = "#979797";
+ document.getElementById("NAVnottop").style.backgroundColor = "#303030";
+ document.getElementById("NAV").style.position = "static";
+}
