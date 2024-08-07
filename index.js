c752bd042d2df53999a22ef9d768fdc481657c75
gperm <gpermweb@gmail.com>
2024-08-07 20:22:04 +0530

    ftdytu

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function NAVnottop() {
  document.getElementById("NAVtop").style.backgroundColor = "#979797";
  document.getElementById("NAVnottop").style.backgroundColor = "#303030";
  document.getElementById("NAV").style.position = "static";
+ document.getElementById("NAV").style.position = " -webkit-static";
 }
 function NAVtop() {
  document.getElementById("NAVnottop").style.color = "#00FFFF";
@@ -15,4 +16,14 @@ function NAVtop() {
  document.getElementById("NAVnottop").style.backgroundColor = "#979797";
  document.getElementById("NAVtop").style.backgroundColor = "#303030";
  document.getElementById("NAV").style.position = "sticky";
+ document.getElementById("NAV").style.position = " -webkit-sticky";
+}
+function pocket() {
+ window.location.assign("pocket.html")
+}
+function magic() {
+ window.location.assign("magic.html")
+}
+function pyraminx() {
+ window.location.assign("pyraminx.html")
 }
