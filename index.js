96430ce4b0e5e33fc10cfca48999e53a4b85d8c5
gperm <gpermweb@gmail.com>
2024-08-22 21:23:17 +0530

    Reboot

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 function web() {
-	window.location.assign("index.html")
+	window.open("index.html","_blank")
+}
+function notation() {
+	window.open("notation.html","_blank")
+}
+function magic() {
+	window.open("magic.html","_blank")
+}
+function pocket() {
+	window.open("pocket.html","_blank")
+}
+function pyraminx() {
+	window.open("pyraminx.html","_blank")
 }
 function contact() {
-	window.location.assign("https://forms.gle/ZEfEqayESopzo2AE9")
+	window.open("https://forms.gle/guFuocEGUjvGn8Ef9","_blank")
 }
 
 function Navtop() {
-	document.getElementById("nav").style.position = "sticky";
-	document.getElementById("nav").style.position = " -webkit-sticky";
+	document.getElementById("Nav").style.position = "sticky";
+	document.getElementById("Nav").style.position = " -webkit-sticky";
 	document.getElementById("Navtop").style.backgroundColor = " #111111";
 	document.getElementById("Navtop").style.color = " #EEEEEE";
 	document.getElementById("Navnottop").style.backgroundColor = " #EEEEEE";
 	document.getElementById("Navnottop").style.color = " #111111";
 }
 function Navnottop() {
-	document.getElementById("nav").style.position = "static";
-	document.getElementById("nav").style.position = " -webkit-static";
+	document.getElementById("Nav").style.position = "static";
+	document.getElementById("Nav").style.position = " -webkit-static";
 	document.getElementById("Navtop").style.backgroundColor = " #EEEEEE";
 	document.getElementById("Navtop").style.color = " #111111";
 	document.getElementById("Navnottop").style.backgroundColor = " #111111";
